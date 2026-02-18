1883a56580645547c5632de8db006b88641c068f
belema7 <belemagirma31@gmail.com>
2026-02-18 09:07:46 +0300

    feat: add RoastOutput component with animated AI response rendering

---
+++ components/RoastOutput.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from 'react';
+import Typewriter from './Typewriter';
+
+interface RoastOutputProps {
+    roast: string;
+}
+
+const RoastOutput: React.FC<RoastOutputProps> = ({ roast }) => {
+    return (
+        <div className="mt-10 border-l-2 border-alert/50 pl-6 py-4 bg-alert/5 relative">
+            <div className="absolute top-0 right-0 p-2 text-[10px] text-alert/30 uppercase">
+                AI_ROAST_GENERATED
+            </div>
+            <div className="text-alert font-bold text-lg mb-4 glitch-text">
+                &gt; DIAGNOSTIC_ANALYSIS_COMPLETE:
+            </div>
+            <div className="text-primary/90 leading-relaxed italic">
+                <Typewriter text={roast} speed={15} />
+            </div>
+        </div>
+    );
+};
+
+export default RoastOutput;
